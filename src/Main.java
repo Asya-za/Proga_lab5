a4410d0050f29420a27b2ad082cabea573100b03
Lena <kadilova08@mail.ru>
2026-02-14 13:07:37 +0300

    one more time

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public void main (){
-    System.out.println("89980");
+    System.out.println("jkhkh");
 }
 
 
