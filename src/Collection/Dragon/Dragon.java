4d3c072dca5902d7df1c3e1b7ed5b18df76ae30c
Lena <kadilova08@mail.ru>
2026-02-14 13:45:43 +0300

    Создали класс Dragon

---
+++ src/Collection/Dragon/Dragon.java
@@ -0,0 +1,13 @@
+package Collection.Dragon;
+
+public class Dragon {
+    private Integer id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
+    private java.time.LocalDateTime creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    private long age; //Значение поля должно быть больше 0
+    private double weight; //Значение поля должно быть больше 0
+    private DragonType type; //Поле не может быть null
+    private DragonCharacter character; //Поле не может быть null
+    private DragonHead head;
+}
